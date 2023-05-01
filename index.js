8e3cf3ed8c2783791dedd1ad5cf99db03de1e992
voronovanasta <110159859+voronovanasta@users.noreply.github.com>
2023-05-02 01:32:28 +0300

    feat: add createKeys function

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ textarea.cols = 50;
 
 body.append(textarea);
 
-//wrapper for keyboard
+/*//wrapper for keyboard
 
 const keyboardWrapper = document.createElement('div');
 keyboardWrapper.className = 'keyboard-wrapper';
@@ -23,7 +23,7 @@ const keyboardKeys = document.createElement('div');
 keyboardKeys.className = 'keyboard-keys';
 keyboardWrapper.append(keyboardKeys);
 
-/*//button for check styles
+button for check styles
 
 //regular button
 const button = document.createElement('button');
@@ -58,6 +58,11 @@ const Keyboard = {
     keys: []
   },
 
+  eventHandlers: {
+    oninput: null,
+    onclose: null
+},
+
   properties: {
     value: "",
     capsLockMode: false,
@@ -69,13 +74,125 @@ const Keyboard = {
     this.elements.wrapper.className = 'keyboard-wrapper'
 
     this.elements.keysContainer = document.createElement('div');
-    this.elements.keysContainer.className = 'keyboard-keys'
+    this.elements.keysContainer.className = 'keyboard-keys';
 
+    this.elements.keysContainer.append(this.createKeys())
     this.elements.wrapper.append(this.elements.keysContainer);
     document.body.append(this.elements.wrapper)
   },
+
+  triggerEvent(EventName){
+
+  },
   
   createKeys(){
+    let countKey = 0;
+    const fragment = document.createDocumentFragment();
+    const keyLayout = [
+            "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
+            "Tab","q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "Del",
+            "CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'",  "Enter",
+            "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "&#9650;", "Shift",
+            "Ctrl", "Win", "Alt", "Space", "Alt", "&#129080;", "&#9660;", "&#129082;", "Ctrl"
+        ];
+
+    keyLayout.forEach(key=>{
+      const keyBtn = document.createElement('button');
+      keyBtn.classList.add('keybord-key');
+      //if not -1- insert a new line
+      const newLine = ["Backspace", "Del", "Enter"].indexOf(key)
+
+      switch(key){
+        case "Backspace":
+          keyBtn.classList.add('keybord-key-wide')
+          keyBtn.innerHTML= "Backspace"
+
+          keyBtn.addEventListener('click', ()=>{
+            this.properties.value=this.properties.value.substring(0, this.properties.value.length-1)
+            this.triggerEvent('oninput')
+          })
+          break;
+
+        case "CapsLock":
+            keyBtn.classList.add('keybord-key-wide')
+            keyBtn.innerHTML= "CapsLock";
+            keyBtn.addEventListener('click', ()=>{
+              this.activateCapsLock();
+            })
+          break;
+        
+          case "Shift":
+            keyBtn.classList.add('keybord-key-wide')
+            keyBtn.innerHTML= "Shift";
+            keyBtn.addEventListener('mousedown', ()=>{
+              this.activateCapsLock();
+            })
+            keyBtn.addEventListener('mouseup', ()=>{
+              this.activateCapsLock();
+            })
+          break;
+
+        case "Enter":
+            keyBtn.classList.add('keybord-key-middle')
+            keyBtn.innerHTML= "Enter";
+            keyBtn.addEventListener('click', ()=>{
+              this.properties.value+='\n';
+              this.triggerEvent('oninput')
+            })
+        break;
+
+        case "Space":
+            keyBtn.classList.add('keybord-key-the-widest')
+            keyBtn.innerHTML= "";
+            keyBtn.addEventListener('click', ()=>{
+              this.properties.value+=' ';
+              this.triggerEvent('oninput')
+            })
+        break;
+
+        case "Tab":
+          keyBtn.classList.add('keybord-key-tab-del')
+          keyBtn.innerHTML= "Tab";
+          keyBtn.addEventListener('click', ()=>{
+            this.properties.value+=' ';
+            this.triggerEvent('oninput')
+          })
+      break;
+        default:
+          keyBtn.textContent = key;
+
+          keyBtn.addEventListener('click',()=>{
+            if(this.properties.capsLockMode){
+              this.properties.value+=key.toLocaleUpperCase();
+            }
+            else{
+              this.properties.value+=key.toLocaleLowerCase();
+  
+            }
+            this.triggerEvent('oninput')
+
+          })
+      }
+      fragment.append(keyBtn)
+
+      if(newLine>=0){
+        fragment.append(document.createElement('br'))
+        console.log(keyLayout.indexOf(key))
+        console.log(key)
+      }
+
+      countKey++
+
+
+      if(countKey === 55){
+        fragment.append(document.createElement('br'))
+      }
+
+
+    })
+
+    return fragment
+
 
   },
 
@@ -85,6 +202,9 @@ const Keyboard = {
   
 }
 
-const init = Keyboard.init.bind('Keyboard')
+window.addEventListener("DOMContentLoaded", function () {
+  Keyboard.init();
+});
 
-document.addEventListener('DOMContentLoaded', init())
+
+console.log('&#129080;')
